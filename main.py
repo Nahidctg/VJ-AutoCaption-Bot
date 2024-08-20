ea52dd865cbd75009f3abe7aa2d5298f9e103435
Nahidctg <164381234+Nahidctg@users.noreply.github.com>
2024-08-20 20:40:33 +0600

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ try: api_hash = os.environ.get("api_hash", "47578d538a0a3a99f8d7bff28cf45c11")
 except Exception as api_id: print(f"⚠️ Api Hash Invalid {api_hash}")
 try: bot_token = os.environ.get("bot_token", "7361786520:AAF7FWD1B0JrsEMUV_F8DZGF-N8ULh-CudU")
 except Exception as bot_token: print(f"⚠️ Bot Token Invalid {bot_token}")
-try: custom_caption = os.environ.get("custom_caption", "`{file_name}`")
+try: custom_caption = os.environ.get("custom_caption", "`{file_name}`Join @CtgMovie270")
 except Exception as custom_caption: print(f"⚠️ Custom Caption Invalid {custom_caption}")
 
 AutoCaptionBotV1 = pyrogram.Client(
@@ -20,14 +20,14 @@ start_message = """
 <b>👋Hello {}</b>
 <b>I am an AutoCaption bot</b>
 <b>All you have to do is add me to your channel and I will show you my power</b>
-<b>@VJ_Botz</b>"""
+<b>@CtgMovie270</b>"""
 
 about_message = """
-<b>• Name : <a href=https://t.me/VJ_Botz>VJ AutoCaption</a></b>
-<b>• Developer : <a href=https://t.me/VJ_Botz>[VJ UPDATES]</a></b>
+<b>• Name : <a href=https://t.me/CtgMovie270>VJ AutoCaption</a></b>
+<b>• Developer : <a href=https://t.me/CtgMovie270>[VJ UPDATES]</a></b>
 <b>• Language : Python3</b>
 <b>• Library : Pyrogram v{version}</b>
-<b>• Updates : <a href=https://t.me/VJ_Botz>Click Here</a></b>
+<b>• Updates : <a href=https://t.me/CtgMovie270>Click Here</a></b>
 <b>• Source Code : <a href=https://github.com/VJBots/VJ-AutoCaption-Bot>Click Here</a></b>"""
 
 @AutoCaptionBotV1.on_message(pyrogram.filters.private & pyrogram.filters.command(["start"]))
@@ -47,7 +47,7 @@ def about_callback(bot, update):
 def edit_caption(bot, update: pyrogram.types.Message):
   motech, _ = get_file_details(update)
   try:
-      try: update.edit(custom_caption.format(file_name=motech.file_name))
+      try: update.edit(custom_caption.format(file_name=motech.file_ame))
       except pyrogram.errors.FloodWait as FloodWait:
           asyncio.sleep(FloodWait.value)
           update.edit(custom_caption.format(file_name=motech.file_name))
@@ -77,10 +77,10 @@ def get_file_details(update: pyrogram.types.Message):
 def start_buttons(bot, update):
   bot = bot.get_me()
   buttons = [[
-   pyrogram.types.InlineKeyboardButton("Updates", url="t.me/VJ_Botz"),
+   pyrogram.types.InlineKeyboardButton("Updates", url="t.me/CtgMovie270"),
    pyrogram.types.InlineKeyboardButton("About 🤠", callback_data="about")
    ],[
-   pyrogram.types.InlineKeyboardButton("➕️ Add To Your Channel ➕️", url=f"http://t.me/{bot.username}?startchannel=true")
+   pyrogram.types.InlineKeyboardButton("➕️ Add To Your Channel ➕️", url=f"http://t.me/{bot.ProAutoCaptionBot}?startchannel=true")
    ]]
   return pyrogram.types.InlineKeyboardMarkup(buttons)
 
